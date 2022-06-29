dbcbdf882d9512eacd7c34501550ebab590692d2
FlorjanKlancar <klancar.florjan99@gmail.com>
2022-06-29 12:54:06 +0200

    middleware change

--- _middleware.js
+++ _middleware.js
@@ -1,7 +1,7 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(req: NextRequest, res: NextResponse) {
+export async function middleware(req, res) {
   if (req.nextUrl.pathname === "/") {
     const session = await getToken({
       req,
